5c5f8b5abcccce9f9b21bf60d69a0a82f67905a9
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-04-18 22:05:40 -0300

    Inicio teste category

---
+++ src/test/java/com/fullcycle/CatalogoVideo/infrastructure/mysql/MySQLCategoryRepositoryImplTest.java
@@ -0,0 +1,61 @@
+package com.fullcycle.CatalogoVideo.infrastructure.mysql;
+
+import com.fullcycle.CatalogoVideo.domain.entity.Category;
+import com.fullcycle.CatalogoVideo.infrastructure.data.SpringDataCategoryRepository;
+import com.fullcycle.CatalogoVideo.infrastructure.persistence.CategoryPersistence;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public class MySQLCategoryRepositoryImplTest {
+
+    @InjectMocks
+    private MySQLCategoryRepositoryImpl service;
+
+    @Mock
+    private SpringDataCategoryRepository repository;
+
+    public void saveCategory(){
+        Category expected = new Category("Action", "Action Description", true);
+        Category input = new Category("Action", "Action Description", true);
+
+        doReturn(CategoryPersistence.from(expected))
+                .when(repository).save(any(CategoryPersistence.class));
+        
+        Category actual = service.create(input);
+
+        assertThat(actual).isNotNull();
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+        assertThat(actual).hasFieldOrPropertyWithValue("name", "Action");
+        assertTrue(actual.getActive());
+    }
+
+    @Test
+    public void findAllCategories(){
+        Category entity1 = new Category("Action", "Action Description", true);
+        Category entity2 = new Category("Action2", "Action Description2", true);
+        List<CategoryPersistence> expected = new ArrayList<>();
+        expected.add(CategoryPersistence.from(entity1));
+        expected.add(CategoryPersistence.from(entity2));
+
+        doReturn(expected).when(repository).findAll();
+
+        List<Category> actual = service.findAll();
+
+        assertThat(actual).isNotNull();
+     }
+}
