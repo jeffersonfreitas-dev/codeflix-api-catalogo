b6ffdd23bf8877b6251061f687dd371ae65757ce
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-04-22 21:31:54 -0300

    Create Class Error Advice

---
+++ src/main/java/com/fullcycle/CatalogoVideo/api/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.fullcycle.CatalogoVideo.api.configuration;
+
+import com.fullcycle.CatalogoVideo.application.exception.NotFoundException;
+import com.fullcycle.CatalogoVideo.domain.exception.DomainException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {NotFoundException.class})
+    public ResponseEntity<?> handleNotFoundException(NotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(value = {DomainException.class})
+    public ResponseEntity<?> handleDomainException(DomainException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND)
+    }
+}
